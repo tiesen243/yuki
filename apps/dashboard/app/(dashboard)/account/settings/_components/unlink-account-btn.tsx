cabf5564f8b5a4594257e9daa7947be62fe21077
tiesen243 <ttien56906@gmail.com>
2024-10-07 19:59:47 +0700

    feat: 🎸 unlink oauth account
    add unlink discord account

---
+++ apps/dashboard/app/(dashboard)/account/settings/_components/unlink-account-btn.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+
+import { Button } from '@yuki/ui/button'
+
+import { api } from '@/lib/trpc/react'
+
+export const UnlinkAccountBtn: React.FC = () => {
+  const router = useRouter()
+  const { mutate, isPending } = api.auth.unlinkDiscord.useMutation({
+    onSuccess: () => router.refresh(),
+  })
+  return (
+    <Button size="sm" onClick={() => mutate()} disabled={isPending}>
+      Unlink Account
+    </Button>
+  )
+}
