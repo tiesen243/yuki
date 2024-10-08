a314e69dc14b3c7b08d1e7b649c66c172c52a90e
tiesen243 <ttien56906@gmail.com>
2024-10-08 17:18:04 +0700

    feat: update product card component imports
    Refactor the product card component to use named imports for Card,
    CardDescription, and CardTitle from '@yuki/ui/card'. This improves
    readability and consistency in the codebase.

--- apps/web/app/_components/product-card.tsx
+++ apps/web/app/_components/product-card.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 import { cn } from '@yuki/ui'
 import { Badge } from '@yuki/ui/badge'
-import * as card from '@yuki/ui/card'
+import { Card, CardDescription, CardTitle } from '@yuki/ui/card'
 
 import { slugify } from '@/lib/utils'
 
@@ -13,7 +13,7 @@ interface ProductCardProps {
 }
 
 export const ProductCard: React.FC<ProductCardProps> = ({ product, className }) => (
-  <card.Card className={cn('group/product aspect-square overflow-hidden', className)} asChild>
+  <Card className={cn('group/product aspect-square overflow-hidden', className)} asChild>
     <Link href={`/p/${slugify(product.name, product.id)}`}>
       <Image
         src={product.image}
@@ -29,11 +29,11 @@ export const ProductCard: React.FC<ProductCardProps> = ({ product, className }) 
         <div className="flex w-full flex-col items-start gap-1 bg-secondary p-4 transition-colors ease-linear group-hover/product:bg-secondary/90">
           <div className="flex items-center gap-2">
             <Badge>{product.category.name}</Badge>
-            <card.CardTitle className="line-clamp-1">{product.name}</card.CardTitle>
+            <CardTitle className="line-clamp-1">{product.name}</CardTitle>
           </div>
-          <card.CardDescription>$ {product.price.toFixed(2)}</card.CardDescription>
+          <CardDescription>$ {product.price.toFixed(2)}</CardDescription>
         </div>
       </div>
     </Link>
-  </card.Card>
+  </Card>
 )
