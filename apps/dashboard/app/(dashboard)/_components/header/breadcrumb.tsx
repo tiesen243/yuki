2846165bc7bec16238fe8fa933f9fc1e5ea3bfa9
tiesen243 <ttien56906@gmail.com>
2024-10-22 00:19:08 +0700

    feat(dashboard): add breadcumb to dashboard header

--- apps/dashboard/app/(dashboard)/_components/header/breadcrumb.tsx
+++ apps/dashboard/app/(dashboard)/_components/header/breadcrumb.tsx
@@ -2,21 +2,34 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useMemo } from 'react'
 
 import { Button } from '@yuki/ui/button'
 import { ChevronRightIcon } from '@yuki/ui/icons'
 
 export const Breadcrumb: React.FC = () => {
   const pathName = usePathname().split('/').filter(Boolean)
+  const renderPathName = useMemo(() => {
+    if (pathName.length > 3) return ['...', ...pathName.slice(-2)]
+    return pathName
+  }, [pathName])
 
   return (
     <ul className="flex items-center gap-1">
-      {pathName.map((name, idx) => (
+      {pathName.length > 0 && (
+        <li className="flex items-center gap-1">
+          <Button variant="secondary" className="h-6 rounded-md px-2" asChild>
+            <Link href="/">~</Link>
+          </Button>
+          {pathName.length > 0 && <ChevronRightIcon />}
+        </li>
+      )}
+      {renderPathName.map((name, idx) => (
         <li key={idx} className="flex items-center gap-1">
           <Button variant="secondary" className="h-6 rounded-md px-2" asChild>
-            <Link href={`/${pathName.slice(0, idx + 1).join('/')}`}>{name}</Link>
+            <Link href={`/${pathName.slice(0, pathName.indexOf(name) + 1).join('/')}`}>{name}</Link>
           </Button>
-          {idx < pathName.length - 1 && <ChevronRightIcon />}
+          {idx < renderPathName.length - 1 && <ChevronRightIcon />}
         </li>
       ))}
     </ul>
