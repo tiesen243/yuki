4cf06780139329c85fa8ee5254bbe7dcbb47a904
tiesen243 <ttien56906@gmail.com>
2024-10-24 22:20:33 +0700

    chore(web): remove search form

--- apps/web/app/(shop)/p/page.tsx
+++ apps/web/app/(shop)/p/page.tsx
@@ -3,12 +3,10 @@ import type { NextPage } from 'next'
 import type { Query } from '@yuki/api'
 
 import { seo } from '@/lib/seo'
-import { SearchForm } from './_search-form'
 import { PageClient } from './page.client'
 
 const Page: NextPage<{ searchParams: Query }> = ({ searchParams }) => (
   <main className="container grid flex-1 grid-cols-1 gap-4 py-4 md:grid-cols-4">
-    <SearchForm searchParams={searchParams} />
     <PageClient searchParams={searchParams} />
   </main>
 )
