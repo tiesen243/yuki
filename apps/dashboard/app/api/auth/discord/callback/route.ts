2b3b1fee931511f9f2066114f4e7ce23b765e9c1
tiesen243 <ttien56906@gmail.com>
2024-11-12 05:59:22 +0700

    fix: remove edge runtime and update db:generate cache
    - Removed `runtime = 'edge'` from discord and trpc routes.
    - Updated `db:generate` cache setting to true in turbo.json.

--- apps/dashboard/app/api/auth/discord/callback/route.ts
+++ apps/dashboard/app/api/auth/discord/callback/route.ts
@@ -2,6 +2,4 @@ import { discordCallback } from '@yuki/auth/oauth'
 
 import { getBaseUrl } from '@/lib/utils'
 
-export const runtime = 'edge'
-
 export const GET = discordCallback(`${getBaseUrl()}/api/auth/discord/callback`)
