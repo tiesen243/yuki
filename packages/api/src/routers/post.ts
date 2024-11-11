bda9086f44e46499174460fa705d700b2860a860
tiesen243 <ttien56906@gmail.com>
2024-11-11 07:00:49 +0700

    fix(build): remove error route

--- packages/api/src/routers/post.ts
+++ packages/api/src/routers/post.ts
@@ -1,28 +1,7 @@
-import { createTRPCRouter, protectedProcedure, publicProcedure } from '@yuki/api/trpc'
-import { createPost } from '@yuki/api/validators/post'
+import { createTRPCRouter, publicProcedure } from '@yuki/api/trpc'
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure.query(async () => {
     return 'Hello World'
-  }),
-
-  getLatestPost: protectedProcedure.query(async ({ ctx }) => {
-    const post = await ctx.db.post.findFirst({
-      where: { authorId: ctx.session.userId },
-      orderBy: { createdAt: 'desc' },
-    })
-
-    return post ?? null
-  }),
-
-  createPost: protectedProcedure.input(createPost).mutation(async ({ input, ctx }) => {
-    const post = await ctx.db.post.create({
-      data: {
-        content: input.content,
-        author: { connect: { id: ctx.session.userId } },
-      },
-    })
-
-    return post
   }),
 })
