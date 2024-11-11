166c7be2e384044b30c1c83f7a656af4e1e4bb24
tiesen243 <ttien56906@gmail.com>
2024-11-12 06:05:37 +0700

    fix(auth): correct redirect logic in AuthLayout
    The redirect logic in the AuthLayout component was incorrect. It was
    redirecting when there was no session, but it should redirect when
    there is a session. This change fixes that logic.

--- apps/dashboard/app/(auth)/layout.tsx
+++ apps/dashboard/app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import { auth } from '@yuki/auth'
 
 const AuthLayout: React.FC<React.PropsWithChildren> = async ({ children }) => {
   const session = await auth()
-  if (!session) redirect('/')
+  if (session) redirect('/')
 
   return <main className="grid min-h-dvh place-items-center">{children}</main>
 }
