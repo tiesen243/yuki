8432ac78ad99077724fc3c7c18dda24b7fbcd087
tiesen243 <ttien56906@gmail.com>
2024-11-12 05:40:14 +0700

    fix(middleware): remove redundant public path checks
    Removed unnecessary checks for public paths in the middleware function.
    This simplifies the code and improves readability. The checks for
    session and user role remain intact to ensure proper access control.

--- apps/dashboard/app/(auth)/layout.tsx
+++ apps/dashboard/app/(auth)/layout.tsx
@@ -1,4 +1,11 @@
+import { redirect } from 'next/navigation'
+
+import { auth } from '@yuki/auth'
+
 const AuthLayout: React.FC<React.PropsWithChildren> = async ({ children }) => {
+  const session = await auth()
+  if (!session) redirect('/')
+
   return <main className="grid min-h-dvh place-items-center">{children}</main>
 }
 
--- apps/dashboard/middleware.ts
+++ apps/dashboard/middleware.ts
@@ -3,15 +3,9 @@ import { NextResponse } from 'next/server'
 
 import { uncachedAuth } from '@yuki/auth/uncached'
 
-const publicPaths = ['/sign-in', '/sign-up', '/forgot-password', '/forgot-password/reset']
-
 export const middleware = async (req: NextRequest) => {
   const session = await uncachedAuth()
   const pathName = new URL(req.url).pathname
-
-  if (!session && !publicPaths.includes(pathName))
-    return NextResponse.redirect(new URL('/sign-in', req.url))
-  if (session && publicPaths.includes(pathName)) return NextResponse.redirect(new URL('/', req.url))
 
   if (pathName.startsWith('/admin') && session?.user.role !== 'ADMIN')
     return NextResponse.redirect(new URL('/', req.url))
