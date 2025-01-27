d2ade29bb48c6193c788692858bcf0935f0a6424
tiesen243 <ttien56906@gmail.com>
2025-01-27 20:54:04 +0700

    feat: custimize og image

--- apps/web/app/api/og/route.tsx
+++ apps/web/app/api/og/route.tsx
@@ -1,13 +1,15 @@
 import type { NextRequest } from 'next/server'
 import { ImageResponse } from 'next/og'
 
+import { createMetadata } from '@/lib/metadata'
+
 export function GET(req: NextRequest) {
   const { searchParams } = new URL(req.url)
 
-  const title = searchParams.get('title') ?? 'Tiesen'
-  const description =
-    searchParams.get('description') ??
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  const metadata = createMetadata({})
+  const title = searchParams.get('title') ?? 'Welcome to Yuki'
+  const description = searchParams.get('description') ?? metadata.description
+  const image = searchParams.get('image') ?? ''
 
   return new ImageResponse(
     (
@@ -17,28 +19,43 @@ export function GET(req: NextRequest) {
           backgroundColor: '#0c0c0c',
           backgroundImage: `linear-gradient(to top right, hsl(221,89%,72%), transparent)`,
         }}
-        tw="flex flex-col w-full h-full p-12 text-white"
+        tw="flex w-full h-full p-12 text-white items-center"
       >
-        <div tw="flex flex-row items-center mb-3 text-white">
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src={`https://tiesen.id.vn/assets/logo.svg`}
-            tw="w-20 h-20 mr-4"
-            style={{ filter: 'invert(1)' }}
-          />
+        <div tw={`flex flex-col ${image ? 'w-2/3' : 'w-full'}`}>
+          <div tw="flex flex-row items-center mb-3 text-white">
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src={`https://tiesen.id.vn/assets/logo.svg`}
+              tw="w-20 h-20 mr-4"
+              style={{ filter: 'invert(1)' }}
+            />
 
-          <p style={{ fontSize: '56px', fontWeight: 600 }}>Tiesen</p>
+            <p style={{ fontSize: '56px', fontWeight: 600 }}>
+              {metadata.applicationName}
+            </p>
+          </div>
+
+          <p
+            style={{
+              fontWeight: 800,
+              fontSize: '48px',
+            }}
+          >
+            {title}
+          </p>
+          <p style={{ fontSize: '32px', color: 'rgba(240,240,240,0.8)' }}>
+            {description}
+          </p>
         </div>
 
-        <p
-          style={{
-            fontWeight: 800,
-            fontSize: '48px',
-          }}
-        >
-          {title}
-        </p>
-        <p style={{ fontSize: '32px', color: 'rgba(240,240,240,0.8)' }}>{description}</p>
+        {image && (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img
+            src={image}
+            tw="w-1/3 rounded-lg"
+            style={{ objectFit: 'cover', aspectRatio: '1/1' }}
+          />
+        )}
       </div>
     ),
     {
