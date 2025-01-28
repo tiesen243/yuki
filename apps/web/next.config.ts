d7fb3de8e1ce8e89ea36458f1389ae1b5aff6168
tiesen243 <ttien56906@gmail.com>
2025-01-29 05:37:18 +0700

    fix(cors): wish it work

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -23,17 +23,15 @@ const nextConfig: NextConfig = {
       {
         source: '/api/:path*',
         headers: [
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: 'https://yuki-dashboard.vercel.app',
-          },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
           {
             key: 'Access-Control-Allow-Methods',
-            value: 'GET, POST, PUT, DELETE, OPTIONS',
+            value: 'GET,DELETE,PATCH,POST,PUT',
           },
           {
             key: 'Access-Control-Allow-Headers',
-            value: 'X-Requested-With, Content-Type, Authorization',
+            value: 'x-trpc-source,X-Requested-With,Content-Type,Authorization',
           },
         ],
       },
