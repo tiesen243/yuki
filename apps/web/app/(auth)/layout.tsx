0846a188b7ed77c5414d6bbfb95cf9f046810812
tiesen243 <ttien56906@gmail.com>
2025-01-29 22:53:11 +0700

    feat(web): oAuth button

--- apps/web/app/(auth)/layout.tsx
+++ apps/web/app/(auth)/layout.tsx
@@ -9,19 +9,21 @@ export default function AuthLayout({
       <Card className="w-full max-w-xl">
         {children}
 
-        <CardFooter className="flex-col gap-2">
-          <span className="text-muted-foreground relative w-full text-center">
-            <div className="bg-muted-foreground absolute top-1/2 left-0 h-px w-[48%]" />
-            or
-            <div className="bg-muted-foreground absolute top-1/2 right-0 h-px w-[48%]" />
-          </span>
-          <Button variant="outline" className="w-full">
-            Continue with Github
-          </Button>
-          <Button variant="outline" className="w-full">
-            Continue with Discord
-          </Button>
-        </CardFooter>
+        <form>
+          <CardFooter className="flex-col gap-2">
+            <span className="text-muted-foreground relative w-full text-center">
+              <div className="bg-muted-foreground absolute top-1/2 left-0 h-px w-[48%]" />
+              or
+              <div className="bg-muted-foreground absolute top-1/2 right-0 h-px w-[48%]" />
+            </span>
+            <Button variant="outline" className="w-full" formAction="/api/auth/github">
+              Continue with Github
+            </Button>
+            <Button variant="outline" className="w-full" formAction="/api/auth/discord">
+              Continue with Discord
+            </Button>
+          </CardFooter>
+        </form>
       </Card>
     </main>
   )
