e10b952f10eb6d411212458b0f5b8cf39231fb62
tiesen243 <ttien56906@gmail.com>
2025-02-01 01:01:19 +0700

    chore: bump deps

--- apps/dashboard/src/App.vue
+++ apps/dashboard/src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 
+import { Toaster } from '@yuki/ui/vue/toast'
+
 import Header from '@/components/header.vue'
 </script>
 
 <template>
   <Header />
   <RouterView />
+  <Toaster />
 </template>
