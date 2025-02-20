9d6a2fefb5420e044a2ac7f0c545a8b61a57990d
tiesen243 <ttien56906@gmail.com>
2025-02-20 18:56:19 +0700

    chore(dashboard): remove 'use client' to hide warning in build

--- apps/dashboard/app/lib/trpc/react.tsx
+++ apps/dashboard/app/lib/trpc/react.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import type { QueryClient } from '@tanstack/react-query'
 import { useState } from 'react'
 import { QueryClientProvider } from '@tanstack/react-query'
--- apps/dashboard/app/routes/_index.tsx
+++ apps/dashboard/app/routes/_index.tsx
@@ -1,14 +1,19 @@
 import { useQuery } from '@tanstack/react-query'
 
+import type { Route } from './+types/_index'
 import { useTRPC } from '@/lib/trpc/react'
+import { getQueryClient, trpc } from '@/lib/trpc/server'
 
-export default function HomePage() {
+export const loader = async ({ request }: Route.LoaderArgs) => {
+  await getQueryClient().prefetchQuery(
+    trpc(request).product.getAll.queryOptions({}),
+  )
+}
+
+export default function HomePage(_: Route.ComponentProps) {
   const trpc = useTRPC()
+
   const { data } = useQuery(trpc.product.getAll.queryOptions({}))
 
-  return (
-    <main className="container py-4">
-      {JSON.stringify(data?.products, null, 2)}
-    </main>
-  )
+  return <main className="container py-4">{JSON.stringify(data, null, 2)}</main>
 }
