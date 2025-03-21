c8715a6bb323004b38ebe522c97174b41a78461b
tiesen243 <ttien56906@gmail.com>
2025-03-21 15:13:15 +0700

    feat(web): catch not found error

--- apps/web/app/(main)/[slug]/page.tsx
+++ apps/web/app/(main)/[slug]/page.tsx
@@ -22,114 +22,118 @@ export default async function ProductPage({ params }: Props) {
   const { slug } = await params
   const id = getIdFromSlug(slug)
 
-  const queryClient = getQueryClient()
+  try {
+    const queryClient = getQueryClient()
 
-  const [product, { reviews, averageRating }] = await Promise.all([
-    queryClient.ensureQueryData(trpc.product.getOne.queryOptions({ id })),
-    queryClient.ensureQueryData(
-      trpc.product.getProductReviews.queryOptions({ productId: id }),
-    ),
-    queryClient.prefetchQuery(
-      trpc.product.getRelatedProducts.queryOptions({ id }),
-    ),
-  ])
+    const [product, { reviews, averageRating }] = await Promise.all([
+      queryClient.ensureQueryData(trpc.product.getOne.queryOptions({ id })),
+      queryClient.ensureQueryData(
+        trpc.product.getProductReviews.queryOptions({ productId: id }),
+      ),
+      queryClient.prefetchQuery(
+        trpc.product.getRelatedProducts.queryOptions({ id }),
+      ),
+    ])
 
-  return (
-    <>
-      <Script
-        id="json-ld"
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify({
-            '@context': 'https://schema.org',
-            '@type': 'Product',
-            name: product.name,
-            image: product.image,
-            description: product.description,
-            sku: product.id,
-            mpn: product.id,
-            brand: {
-              '@type': 'Brand',
-              name: 'Yuki',
-            },
-            offers: {
-              '@type': 'Offer',
-              url: `${getBaseUrl()}/${slug}`,
-              priceCurrency: 'USD',
-              price: product.price,
-              priceValidUntil: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
-                .toISOString()
-                .split('T')[0],
-              availability:
-                product.stock > 0
-                  ? 'https://schema.org/InStock'
-                  : 'https://schema.org/OutOfStock',
-              seller: {
-                '@type': 'Organization',
+    return (
+      <>
+        <Script
+          id="json-ld"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'Product',
+              name: product.name,
+              image: product.image,
+              description: product.description,
+              sku: product.id,
+              mpn: product.id,
+              brand: {
+                '@type': 'Brand',
                 name: 'Yuki',
               },
-            },
-            aggregateRating:
-              reviews.length > 0
-                ? {
-                    '@type': 'AggregateRating',
-                    ratingValue: averageRating,
-                    reviewCount: reviews.length,
-                  }
-                : undefined,
-            review:
-              reviews.length > 0
-                ? reviews.map((review) => ({
-                    '@type': 'Review',
-                    reviewRating: {
-                      '@type': 'Rating',
-                      ratingValue: review.rating,
-                    },
-                    author: {
-                      '@type': 'Person',
-                      name: review.user.name,
-                    },
-                    reviewBody: review.comment,
-                    datePublished: review.createdAt,
-                  }))
-                : undefined,
-          }),
-        }}
-      />
+              offers: {
+                '@type': 'Offer',
+                url: `${getBaseUrl()}/${slug}`,
+                priceCurrency: 'USD',
+                price: product.price,
+                priceValidUntil: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000)
+                  .toISOString()
+                  .split('T')[0],
+                availability:
+                  product.stock > 0
+                    ? 'https://schema.org/InStock'
+                    : 'https://schema.org/OutOfStock',
+                seller: {
+                  '@type': 'Organization',
+                  name: 'Yuki',
+                },
+              },
+              aggregateRating:
+                reviews.length > 0
+                  ? {
+                      '@type': 'AggregateRating',
+                      ratingValue: averageRating,
+                      reviewCount: reviews.length,
+                    }
+                  : undefined,
+              review:
+                reviews.length > 0
+                  ? reviews.map((review) => ({
+                      '@type': 'Review',
+                      reviewRating: {
+                        '@type': 'Rating',
+                        ratingValue: review.rating,
+                      },
+                      author: {
+                        '@type': 'Person',
+                        name: review.user.name,
+                      },
+                      reviewBody: review.comment,
+                      datePublished: review.createdAt,
+                    }))
+                  : undefined,
+            }),
+          }}
+        />
 
-      <HydrateClient>
-        <main className="container grow py-4">
-          <Suspense fallback={<ProductDetailsSkeleton />}>
-            <ProductDetails id={id} />
-          </Suspense>
-
-          <section className="mt-4">
-            <h2 className="sr-only">Product Reviews Section</h2>
-            <span className="text-2xl font-bold">Product Reviews</span>
-
-            <Suspense fallback={<ProductReviewsSkeleton />}>
-              <ProductReviews id={id} />
+        <HydrateClient>
+          <main className="container grow py-4">
+            <Suspense fallback={<ProductDetailsSkeleton />}>
+              <ProductDetails id={id} />
             </Suspense>
-          </section>
 
-          <section className="mt-4 grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-6">
-            <h2 className="sr-only">Related Products Section</h2>
-            <span className="col-span-full text-2xl font-bold">
-              Related Products
-            </span>
+            <section className="mt-4">
+              <h2 className="sr-only">Product Reviews Section</h2>
+              <span className="text-2xl font-bold">Product Reviews</span>
 
-            <Suspense
-              fallback={Array.from({ length: 12 }).map((_, i) => (
-                <ProductCardSkeleton key={i} />
-              ))}
-            >
-              <RelatedProducts id={id} />
-            </Suspense>
-          </section>
-        </main>
-      </HydrateClient>
-    </>
-  )
+              <Suspense fallback={<ProductReviewsSkeleton />}>
+                <ProductReviews id={id} />
+              </Suspense>
+            </section>
+
+            <section className="mt-4 grid grid-cols-2 gap-4 md:grid-cols-4 lg:grid-cols-6">
+              <h2 className="sr-only">Related Products Section</h2>
+              <span className="col-span-full text-2xl font-bold">
+                Related Products
+              </span>
+
+              <Suspense
+                fallback={Array.from({ length: 12 }).map((_, i) => (
+                  <ProductCardSkeleton key={i} />
+                ))}
+              >
+                <RelatedProducts id={id} />
+              </Suspense>
+            </section>
+          </main>
+        </HydrateClient>
+      </>
+    )
+  } catch {
+    notFound()
+  }
 }
 
 export const generateMetadata = async ({ params }: Props) => {
