bdc5686716219611cb66f658b61bf4c7573049df
tiesen243 <ttien56906@gmail.com>
2025-05-22 00:18:13 +0700

    feat: add home page slider

--- apps/web/app/(main)/page.tsx
+++ apps/web/app/(main)/page.tsx
@@ -1,16 +1,23 @@
 import { api } from '@/lib/trpc/server'
 import { ProductCard } from '../_components/product-card'
+import { Slider } from './page.client'
 
 export default async function IndexPage() {
   const { products } = await api.product.all({ limit: 12 })
 
   return (
-    <main className="container grow space-y-4 py-4">
-      <div className="grid grid-cols-1 gap-4 sm:grid-rows-2 md:grid-cols-3 lg:grid-cols-4">
+    <main className="grow space-y-4 pb-4">
+      <section>
+        <h2 className="sr-only">Featured Products</h2>
+        <Slider />
+      </section>
+
+      <section className="container grid grid-cols-1 gap-4 sm:grid-rows-2 md:grid-cols-3 lg:grid-cols-4">
+        <h2 className="sr-only">Products</h2>
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
-      </div>
+      </section>
     </main>
   )
 }
